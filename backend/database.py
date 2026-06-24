e37e9001b2a193a45310f2977912135293ea6cfd
aparnavv <aparnavv631@gmail.com>
2026-06-24 17:11:31 +0530

    Connect FastAPI to PostgreSQL

--- backend/database.py
+++ backend/database.py
@@ -4,6 +4,12 @@ from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker,declarative_base
 load_dotenv()
 DATABASE_URL=os.getenv("DATABASE_URL")
+if DATABASE_URL.startswith("postgresql://"):
+    DATABASE_URL = DATABASE_URL.replace(
+        "postgresql://",
+        "postgresql+psycopg2://"
+    )
+
 engine=create_engine(DATABASE_URL)
 SessionLocal=sessionmaker(autocommit=False,autoflush=False,bind=engine)
 Base=declarative_base()
